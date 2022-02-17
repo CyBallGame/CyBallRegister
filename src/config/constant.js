df96ce1ac927928cdcdeff08c8550b3f826df943
tunght91 <hoangtung.utc@gmail.com>
2022-02-17 23:42:56 +0700

    fix dev

--- src/config/constant.js
+++ src/config/constant.js
@@ -1,3 +1,3 @@
-export const API_CYBALL = 'https://app-api.cyball.com/api/SignUp'
+export const API_CYBALL = location.href.indexOf('dev') > 0 ? 'http://cyball-api.azurewebsites.net/api/SignUp' : 'https://app-api.cyball.com/api/SignUp'
 export const EMAIL_REGEX = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 export const PASSWORD_REG = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,}$/
