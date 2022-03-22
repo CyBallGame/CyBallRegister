9e5e5b57de0831ed0a11c16f8732c1894172d5cf
Trung Dao <trungdao912@gmail.com>
2022-03-22 09:47:11 +0700

    adding more condition at config file

--- src/configurations/index.js
+++ src/configurations/index.js
@@ -2,7 +2,6 @@ import devConfigs from './dev'
 import prodConfigs from './prod'
 
 const env = process.env.REACT_APP_ENV
-
-const configs = env === 'dev' || location.host.indexOf('localhost') === 0 ? devConfigs : prodConfigs
-
+// If you add #dev at the end of the url, it will choose the dev configuartion
+const configs = env === 'dev' || location.host.indexOf('localhost') === 0 || location.href.indexOf('dev') > 0 ? devConfigs : prodConfigs
 export default Object.freeze({ ...configs })
